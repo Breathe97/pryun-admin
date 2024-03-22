18194ee856b0ff11957af237aee37fd5d761228f
zhaop <704340916@qq.com>
2024-03-22 16:14:59 +0800

    a

--- src/api/core/axios.d.ts
+++ src/api/core/axios.d.ts
@@ -8,7 +8,7 @@ declare module 'axios' {
   // 服务端返回内容
   interface IAxios<D = null> {
     code: string | number
-    message: string
+    msg: string
     data: D
   }
   export interface AxiosResponse<T = any> extends IAxios<D> {}
