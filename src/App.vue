dbbcdaa2ed090b9f20281bec7da67c7e3b203860
zhaop <704340916@qq.com>
2024-03-26 11:39:14 +0800

    aa

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
-  <router-view></router-view>
+  <el-config-provider :locale="locale">
+    <router-view></router-view>
+  </el-config-provider>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import en from 'element-plus/dist/locale/en.mjs'
+
+const language = ref('zh-cn')
+const locale = computed(() => (language.value === 'zh-cn' ? zhCn : en))
+</script>
 <style scoped></style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,24 +1,15 @@
 <template>
-  <el-config-provider :locale="locale">
-    <component :is="Layout">
-      <ContentView></ContentView>
-    </component>
-  </el-config-provider>
+  <component :is="Layout">
+    <ContentView></ContentView>
+  </component>
 </template>
 
 <script lang="ts" setup>
-import { ElConfigProvider } from 'element-plus'
 import LayoutAdmin from './layout-admin/index.vue'
 import LayoutSuper from './layout-admin-tfxq/index.vue'
 import ContentView from './components/ContentView/ContentView.vue'
 
-import { ref, computed } from 'vue'
-
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-import en from 'element-plus/dist/locale/en.mjs'
-
-const language = ref('zh-cn')
-const locale = computed(() => (language.value === 'zh-cn' ? zhCn : en))
+import { computed } from 'vue'
 
 const role = 'super' // 可能需要根据角色来动态显示不同的布局
 
