317009309a22b8a70c7be476a93de6e611033ea1
zhaop <704340916@qq.com>
2024-03-26 17:28:03 +0800

    aaa

---
+++ src/views/workbench/components/card.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card">
+    <div class="card-title">{{ title }}</div>
+    <div class="card-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+  title: {
+    type: [String],
+    default: () => '',
+  },
+})
+</script>
+<style scoped lang="scss">
+.card {
+  border-radius: 12px;
+  overflow: hidden;
+  .card-title {
+    padding: 0 16px;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    background-color: rgba(64, 158, 254, 1);
+
+    color: #ffffff;
+  }
+  .card-content {
+    min-height: 80px;
+    background-color: #ffffff;
+  }
+}
+</style>
--- src/views/workbench/workbench-index.vue
+++ src/views/workbench/workbench-index.vue
@@ -1,42 +1,46 @@
 <template>
   <div style="padding: 20px; box-sizing: border-box">
-    <div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center; color: red" @click="aaa">workbench-index</div>
-      <svg class="pr-icon" aria-hidden="true" style="font-size: 40px">
-        <use :xlink:href="`#pr-icon-${icon}`"></use>
-      </svg>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-      <div style="height: 200px; display: flex; align-items: center; justify-content: center">workbench-index</div>
-    </div>
+    <Card title="销售统计">
+      <div class="card-item">
+        <div class="pr-icon pr-icon-pryun"></div>
+      </div>
+    </Card>
   </div>
 </template>
 <script lang="ts" setup>
 import { ref } from 'vue'
+import Card from './components/card.vue'
+import * as workbenchApi from '@/api/modules/workbench'
 
 const icon = ref('calendar')
 const aaa = () => {
   icon.value = icon.value === 'calendar' ? 'badge' : 'calendar'
 }
+
+const detail = ref({
+  monthCount: 0,
+  todayCount: 0,
+  submitSpu: 0,
+  expressCount: 0,
+  checkReturn: 0,
+  auditReturn: 0,
+  auditRefund: 0,
+})
+
+const getData = () => {
+  workbenchApi.getGoodsSale().then((res) => {
+    console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:res`, res)
+  })
+  workbenchApi.getGoodsStat().then((res) => {
+    console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:res`, res)
+  })
+  workbenchApi.getGoodsStatChart().then((res) => {
+    console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:res`, res)
+  })
+}
+getData()
 </script>
-<style scoped></style>
+<style scoped>
+.aa {
+}
+</style>
